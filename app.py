1c30f036854f2ce9a3f423cdbc0157b14abd15f4
John Miner <john.douglas.miner.iv@gmail.com>
2019-12-02 22:35:52 -0800

    routes

--- app.py
+++ app.py
@@ -3,9 +3,24 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def main():
     return render_template('index.html')
 
+@app.route('/decode')
+def decode():
+    return render_template('decode.html')
+
+@app.route('/encode')
+def encode():
+    return render_template('encode.html')
+
+@app.route('/create')
+def encode():
+    return render_template('create.html')
+
+@app.route('/login')
+def encode():
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
