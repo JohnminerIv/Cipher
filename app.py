166b275d17a021c099eb2191950b3ddb9ebbd583
John Miner <john.douglas.miner.iv@gmail.com>
2019-12-11 10:45:22 -0800

    trying to push to heroku

--- app.py
+++ app.py
@@ -23,4 +23,4 @@ def login():
     return render_template('login.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
